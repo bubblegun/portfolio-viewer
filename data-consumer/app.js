ac10ab57e3750e7a2b0e7b3c395ffde761d53874
bubblegun <bubblegun2k9@gmail.com>
2017-06-22 19:32:25 +0200

    added config file for node

--- data-consumer/app.js
+++ data-consumer/app.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app = express()
+'use strict';
+
+const config    = require('config');
+const express   = require('express');
+const app       = express();
+
+
+if (config.has('sslConfig')) {
+    var sslPW = config.get('sslConfig.password');
+} else {
+    console.log('Can\'t find configuration file!');
+}
+
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+    console.log('Example app listening on port 3000!');
+});
