772ff9c662bb5e3d67cef64c58ace603c4237232
bubblegun <bubblegun2k9@gmail.com>
2017-06-25 20:02:23 +0200

    added mysql

--- data-consumer/app.js
+++ data-consumer/app.js
@@ -1,15 +1,41 @@
 'use strict';
 
 const config    = require('config');
+const mysql     = require('mysql');
 const express   = require('express');
 const app       = express();
 
+var connection = mysql.createConnection({
+  host     : config.get('config.mySQL.server'),
+  user     : config.get('config.mySQL.user'),
+  password : config.get('config.mySQL.password'),
+  database : config.get('config.mySQL.database')
+});
 
-if (config.has('sslConfig')) {
-    var sslPW = config.get('sslConfig.password');
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+connection.query('SELECT * FROM fund',function(err,rows){
+  if(err) throw err;
+
+  console.log('Data received from Db:\n');
+  console.log(rows);
+});
+
+if (config.has('config')) {
+    var sslPW = config.get('config.sslConfig.password');
 } else {
     console.log('Can\'t find configuration file!');
 }
+
+console.log('NODE_ENV: '        + config.util.getEnv('NODE_ENV'));
+console.log('NODE_CONFIG_DIR: ' + config.util.getEnv('NODE_CONFIG_DIR'));
 
 
 app.get('/', function (req, res) {
